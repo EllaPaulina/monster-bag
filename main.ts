649a8c1d478866be21bf7b99176d11fb6298ea09
Obada-Fa <57995628+Obada-Fa@users.noreply.github.com>
2024-04-24 00:38:50 +0200

    fixes

--- main.ts
+++ main.ts
@@ -1,34 +1,38 @@
 namespace SpriteKind {
     export const egg = SpriteKind.create()
     export const cracked1 = SpriteKind.create()
+    export const monster = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.cracked1, function (sprite, otherSprite) {
     if (controller.A.isPressed()) {
         otherSprite.setKind(SpriteKind.cracked1)
-        Egg.setImage(assets.image`crack 2`)
-        sprites.destroy(Egg, effects.spray, 200)
+        otherSprite.setImage(assets.image`crack 2`)
+        sprites.destroy(otherSprite, effects.clouds, 200)
         info.changeScoreBy(1)
         info.changeCountdownBy(1)
-        myEnemy = sprites.create(img`
-            . . f f f . . . . . . . . . . . 
-            f f f c c . . . . . . . . f f f 
-            f f c c c . c c . . . f c b b c 
-            f f c 3 c c 3 c c f f b b b c . 
-            f f c 3 b c 3 b c f b b c c c . 
-            f c b b b b b b c f b c b c c . 
-            c c 1 b b b 1 b c b b c b b c . 
-            c b b b b b b b b b c c c b c . 
-            c b 1 f f 1 c b b c c c c c . . 
-            c f 1 f f 1 f b b b b f c . . . 
-            f f f f f f f b b b b f c . . . 
-            f f 2 2 2 2 f b b b b f c c . . 
-            . f 2 2 2 2 2 b b b c f . . . . 
-            . . f 2 2 2 b b b c f . . . . . 
-            . . . f f f f f f f . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Enemy)
-        pause(200)
-        myEnemy.follow(Main_character, 90)
+        if (info.score() < 9) {
+            Monster = sprites.create(img`
+                . . f f f . . . . . . . . . . . 
+                f f f c c . . . . . . . . f f f 
+                f f c c c . c c . . . f c b b c 
+                f f c 3 c c 3 c c f f b b b c . 
+                f f c 3 b c 3 b c f b b c c c . 
+                f c b b b b b b c f b c b c c . 
+                c c 1 b b b 1 b c b b c b b c . 
+                c b b b b b b b b b c c c b c . 
+                c b 1 f f 1 c b b c c c c c . . 
+                c f 1 f f 1 f b b b b f c . . . 
+                f f f f f f f b b b b f c . . . 
+                f f 2 2 2 2 f b b b b f c c . . 
+                . f 2 2 2 2 2 b b b c f . . . . 
+                . . f 2 2 2 b b b c f . . . . . 
+                . . . f f f f f f f . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.monster)
+            Monster.setPosition(-10, -10)
+            pause(200)
+            Monster.follow(Main_character, 90)
+        }
     }
 })
 function animate_charachter () {
@@ -165,12 +169,12 @@ function animate_charachter () {
 sprites.onOverlap(SpriteKind.Player, SpriteKind.egg, function (sprite, otherSprite) {
     if (controller.A.isPressed()) {
         otherSprite.setKind(SpriteKind.cracked1)
-        Egg.setImage(assets.image`crack 1`)
+        otherSprite.setImage(assets.image`crack 1`)
         pause(100)
     }
 })
-let myEnemy: Sprite = null
 let Egg: Sprite = null
+let Monster: Sprite = null
 let Main_character: Sprite = null
 game.splash("Welcome to Moster Bag")
 tiles.setCurrentTilemap(tilemap`level2`)
@@ -225,7 +229,7 @@ game.onUpdate(function () {
         animate_charachter()
     }
 })
-game.onUpdateInterval(5000, function () {
+game.onUpdateInterval(1000, function () {
     Egg = sprites.create(assets.image`uncracked`, SpriteKind.egg)
     Egg.setPosition(randint(0, 160), randint(0, 160))
 })
